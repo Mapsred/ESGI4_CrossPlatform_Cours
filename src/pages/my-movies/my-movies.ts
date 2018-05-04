a97c86bfdaa8e0d8d915126bcdee91a151ae6568
François MATHIEU <maps_red@yahoo.fr>
2018-05-04 18:36:35 +0200

    Add MyMovies redirection

--- src/pages/my-movies/my-movies.ts
+++ src/pages/my-movies/my-movies.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieListPage } from "../movie-list/movie-list";
 
 /**
  * Generated class for the MyMoviesPage page.
@@ -15,11 +16,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyMoviesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyMoviesPage');
   }
 
+  redirectToMoviesList() {
+    return this.navCtrl.push(MovieListPage);
+  }
 }
